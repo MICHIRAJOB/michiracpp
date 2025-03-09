7bf2121836530ea2e1dd862b246806c0bcd570a5
MICHIRA <157963644+MICHIRAJOB@users.noreply.github.com>
2025-03-09 21:51:28 +0300

    Add files via upload

---
+++ Arrays 2/arrays.cpp
@@ -0,0 +1,21 @@
+//c  ARRAYS
+/*
+c redefined functions
+Name:Job Michira
+Reg no: BBIT-05-0028/2024
+Git username:Michira
+Campus:TRC
+version
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[6]={10,20,30,40,50,60};
+    int i;
+    for(i=0; i<6; i++){
+		 cout<<marks[i] <<"\t";
+		
+    
+    cout<<endl;}
+    return 0;
+}
